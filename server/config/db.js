3b3a8e025ed09d70eb9531ecc7602a648e524590
Divyansh Srivastava <divyanshsri1101@gmail.com>
2026-04-24 11:48:22 +0530

    DB config updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 const MongoDB = () => {
     mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("DB connected Successfully");
+    .then((conn) => {
+        console.log(`DB connected Successfully to: ${conn.connection.name}`);
     })
-    .catch(() => {
-        console.log("DB is not connected");;
-        
-    })
+    .catch((err) => {
+        console.log("DB is not connected. Error:", err.message);
+    });
 }
 
 module.exports = MongoDB;
